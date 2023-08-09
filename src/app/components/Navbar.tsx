ba8a12d05cb60704dcd95755136c1507f4b5205b
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-08-09 17:28:14 -0500

    React Tailwind Responsive Navbar Menu tutorial

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between
+    border-b-2 border-b-red-500 uppercase'>
+      {/*Logo*/}  
+      <div className='text-xl'>
+        <Link href="/"> pizzas kevin</Link>
+      </div>
+      {/*mobile menu */}
+      <div>
+        <Menu />
+      </div>
+    </div>
+      
   )
 }
 
--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Notification() {
   return (
-    <div>Notification</div>
+    <div className="h-12 bg-red-500 text-white px-4 
+    flex items-center text-center text-sm justify-center
+    md:text-base cursor-pointer">Free delivery for all orders over $10. Order your food now!</div>
   )
 }
 
