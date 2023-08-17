1e928906023b4e19997464a18634753c8e79e3a5
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-08-17 15:39:40 -0500

    Product

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -5,15 +5,22 @@ import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div className="flex text-red-500">
+    <div className="flex flex-wrap text-red-500">
       {pizzas.map((item) => (
-        <Link className='w-full h-[600vh] border-r-2 border-b-2 border-red-500' href={`/product/${item.id}`} key={item.id}>
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3
+        flex flex-col justify-between group odd:bg-fuchsia-50' href={`/product/${item.id}`} key={item.id}>
 
            {item.img && (
-            <div className='relative'>
-              <Image src={item.img} alt="" fill/>
+            <div className='relative h-[80%]'>
+              <Image src={item.img} alt="" fill className='object-contain'/>
             </div>
            )}
+
+           <div className='flex items-center justify-between font-bold'>
+            <h1 className='text-2xl uppercase p-2'>{item.title}</h1>
+            <h2 className='group-hover:hidden text-xl'>{item.price}</h2>
+            <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+           </div>
         </Link> 
       ))}
     </div>
